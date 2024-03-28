dc6a3eddd735d948aa16d1eb62077b4de29267ec
Michael Barbosa <31492967+michaelfsb@users.noreply.github.com>
2024-03-27 22:20:57 -0300

    Fix fuel cell model and Add new Grid model

--- src/microgrid_parameters.m
+++ src/microgrid_parameters.m
@@ -3,7 +3,7 @@ load("input_data_24h.mat")
 
 URFC = urfc_parameters;
 PV = photovoltaic_parameters;
-BUS.Voltage = 300;                          % [V]
+BUS.Voltage = 500;                          % [V]
 URFC_TANK.MaxLvl = 1000;                    % [g]
 URFC_TANK.MinLvl = 0.2*URFC_TANK.MaxLvl;    % [g]
 URFC_TANK.IniLvl = 0.7*URFC_TANK.MaxLvl;    % [g]
